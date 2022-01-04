ffc69a523d285f4b391f09c5fef7686c677c9a03
Adrian Lubitz <adrianlubitz@gmail.com>
2022-01-04 18:33:43 +0100

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # nxt-remote
-A python remote for the LEGO NXT
+A python remote for the LEGO NXT using [nxt-python](https://github.com/schodet/nxt-python). This project was developed using python 3.7 on Windows.
+
+## Install
+Follow the [install guide of nxt-python](https://github.com/schodet/nxt-python#requirements) and make sure to properly install *PyUSB* and *PyBluez*. 
+I needed to put the **libusb-1.0.dll** in the *system32* folder to make *PyUSB* work.
+
+
+## Usage
+To start the remote just run
+
+```bash
+python remote.py
+```
+
+## Known Issues
+The Bluetooth connection is not working with my setup of nxt-python under Windows.
---
+++ remote.py
@@ -0,0 +1,52 @@
+'''This is a remote control for the NXT. All keystrokes will be send to the NXT with the corresponding key code'''
+# system imports
+
+# 3rd party imports
+import nxt.locator
+from nxt.error import DirectProtocolError
+from pynput.keyboard import Key, Listener
+
+
+# local imports
+
+
+class Remote():
+    def __init__(self, **kwargs) -> None:
+        self.connect(**kwargs)
+        print('started remote - press ESC to close it.')
+        # wait for keystrokes
+        with Listener(on_press=self.on_keystroke, on_release=self.on_release) as listener:
+            listener.join()
+        
+    def connect(self, **kwargs):
+        if 'host' in kwargs:
+            host = kwargs['host']
+        else:
+            host = "00:16:53:0E:45:FA"
+        try:
+            self.brick = nxt.locator.find(host=host)
+            print(f'connected to {host}')
+        except Exception as e:
+            print(f"Couldn't connect: {e}")
+
+    def on_keystroke(self, key, mailbox=0):
+        '''
+        This callback sends the key code to the NXT
+        '''
+        try:
+            self.brick.message_write(mailbox, bytes(str(key), 'utf-8'))
+        except DirectProtocolError as e:
+            print(e)
+        except AttributeError as e:
+            print('Remote is not connected')
+            #TODO: add some logic to reconnect - maybe a specific button or combi like ctrl+r
+
+
+    def on_release(self, key):
+        if key == Key.esc:
+            # Stop listener
+            return False
+
+if __name__ == '__main__':
+    remote = Remote()
+    # remote.on_keystroke('b')
